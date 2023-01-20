39809edd08ee9d88aefca3bbe96e65c94f121da2
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-20 07:26:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -459,20 +459,19 @@ async def settings(client, message):
              
 @Client.on_message((filters.command(["request", "Request"]) | filters.regex("#request") | filters.regex("#Request")) & filters.group)
 async def requests(bot, message):
-    chat_id = message.chat.id
-    reporter = str(message.from_user.id)
-    mention = message.from_user.mention
-    content = message.text
+    if:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        content = message.text
     
-
-
-    btn = [[
-        
-        InlineKeyboardButton('View Request', url=f"{message.link}"),
-        InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
-    ]]
-    
-    reported_post = await bot.send_message(chat_id=REQUEST_CHANNEL, text=f"<b>𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
+        btn = [[
+            InlineKeyboardButton('View Request', url=f"{message.link}"),
+            InlineKeyboardButton('Show Options', callback_data=f'show_option#{reporter}')
+        ]]
+        await bot.send_message(chat_id=REQUEST_CHANNEL, text=f"<b>𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}</b>", reply_markup=InlineKeyboardMarkup(btn))
+    else:
+        await message.reply_text("<b>Your request has been added! Please wait for some time.</b>")
 
 
         
