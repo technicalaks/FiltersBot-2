41dfc603c85351891c9e289a50d332d078b477b4
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-20 10:20:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Others
-REQUEST_CHANNEL =  = int(environ.get('REQUEST_CHANNEL', ''))
+REQUEST_CHANNEL = int(environ.get('REQUEST_CHANNEL', ''))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/SL_Filters_Bot_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
