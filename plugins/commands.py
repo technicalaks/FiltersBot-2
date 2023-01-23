fa262c29dc7146f7341b144aee3b161459da8bcd
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-23 09:17:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -470,7 +470,7 @@ async def send_request(bot, message):
             buttons = [[
                 InlineKeyboardButton('View Request', url=f"{message.link}")
             ],[
-                InlineKeyboardButton('🔰 Show Options 🔰', callback_data=f'show_options#{message.from_user.id}')
+                InlineKeyboardButton('🔰 Show Options 🔰', callback_data=f'show_options#{message.from_user.id}#{message.id}')
             ]]
             sent_request = await bot.send_message(REQUEST_CHANNEL, script.REQUEST_TXT.format(message.from_user.mention, message.from_user.id, request), reply_markup=InlineKeyboardMarkup(buttons))
             
