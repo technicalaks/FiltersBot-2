ee6371f252e13f4a9fcfc2617d999dcc59f50478
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-24 09:19:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_Search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '13570748'))
+API_HASH = environ.get('API_HASH', '5e7dcf76a539a41177fb5b44f767d069')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5848797829:AAGUelF2Ek6LT0xVCLPlNX_b_y5uu-2g3MI')
 
 # Bot settings
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
@@ -22,29 +22,28 @@ STICKERS = (environ.get('STICKERS', 'CAACAgIAAxkBAAEGm9hjhf69CtQmXoeQ2HidYCGBFeZ
 PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5493832202').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001396095544 -1001622478032 -1001566642237 -1001805305525 -1001773156985').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '')
-auth_grp = environ.get('AUTH_GROUP', '')
-support_group = environ.get('SUPPORT_GROUP', '')
-request_group = environ.get('REQUEST_GROUP', '')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001791560305')
+auth_grp = environ.get('AUTH_GROUP')
+support_group = environ.get('SUPPORT_GROUP', '-1001305756928')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 SUPPORT_GROUP = int(support_group) if support_group and id_pattern.search(support_group) else None
-REQUEST_GROUP = int(request_group) if request_group and id_pattern.search(request_group) else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hansaka:anuhas@cluster0.gpb2fz7.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+REQUEST_CHANNEL = int(environ.get('REQUEST_CHANNEL', '-1001862488332'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001701705208'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/SL_Filters_Bot_Support')
-AUTOFILTER = is_enabled((environ.get('AUTOFILTER', "True")), True)
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "False")), False)
+AUTO_FILTER = is_enabled((environ.get('AUTO_FILTER', "True")), True)
+SEND_FILE_CHANNEL = is_enabled((environ.get('SEND_FILE_CHANNEL', "False")), False)
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
