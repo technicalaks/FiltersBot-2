f6afa95d09c8b801f4b90461dea3ead29d792feb
Hansaka Anuhas <121796740+HansakaAnuhas@users.noreply.github.com>
2023-01-24 09:23:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -7,7 +7,7 @@ from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidD
 from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, make_inactive
-from info import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, SUPPORT_CHAT, SUPPORT_GROUP, PICS, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, PROTECT_CONTENT, IMDB, SINGLE_BUTTON, SPELL_CHECK, IMDB_TEMPLATE, AUTO_FILTER, SEND_FILE_CHANNEL
+from info import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, SUPPORT_CHAT, SUPPORT_GROUP, PICS, AUTH_USERS, CUSTOM_FILE_CAPTION, AUTH_GROUPS, P_TTI_SHOW_OFF, PROTECT_CONTENT, IMDB, SINGLE_BUTTON, SPELL_CHECK_REPLY, IMDB_TEMPLATE, AUTO_FILTER, SEND_FILE_CHANNEL
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid, ChatAdminRequired
